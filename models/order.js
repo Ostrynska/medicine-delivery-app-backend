170ea67581a51784c0dd7ef6a256a569bacd25ee
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-03-07 13:47:36 +0200

    fix

--- models/order.js
+++ models/order.js
@@ -9,36 +9,60 @@ const orderShema = new Schema(
     },
     name: {
         type: String,
+        required: [true, 'Name field is required'],
     },
     email: {
         type: String,
+        required: [true, 'Email field is required'],
     },
     phone: {
         type: String,
-    },
-    drugslist: [
-        {
-            shop: {
-                type: String
-            },
-            drug: {
-                type: String,
-            },
-            price: {
-                type: Number,
-            },
-            quantity: {
-                type: Number,
-            },
-            totalPrice: {
-                type: Number,
-            },
-        }
-    ],
-}, {versionKey: false, timestamps: true});
+        required: [true, 'Phone field is required'],
+        },
+    drugslist: {
+        type: Schema.Types.ObjectId,
+        ref: "shops",
+        required: true,
+    }
+    // drugslist: [
+    //     {
+    //         shop: {
+    //             type: String
+    //         },
+    //         drug: {
+    //             type: String,
+    //         },
+    //         price: {
+    //             type: Number,
+    //         },
+    //         quantity: {
+    //             type: Number,
+    //         },
+    //         totalPrice: {
+    //             type: Number,
+    //         },
+    //     }
+    // ], required: true,
+    }, { versionKey: false, timestamps: true });
+
+const addSchema = Joi.object({
+    name: Joi.string().min(3).max(30).required(),
+    email: Joi.string().email().lowercase().required(),
+    phone: Joi.string().required(),
+    // favorite: Joi.boolean()
+})
+
+// const updateFavoriteSchema = Joi.object({
+//     favorite: Joi.boolean().required(),
+// })
+
+const schemas = {
+    addSchema,
+    // updateFavoriteSchema,
+}
 
 orderShema.post("save", handleMongooseError);
 
 const Order = model('order', orderShema);
 
-module.exports = Order;
+module.exports = {Order, schemas};
--- routes/api/orders.js
+++ routes/api/orders.js
@@ -5,7 +5,10 @@ const router = express.Router();
 const ctrl = require('../../controllers/orders');
 const { validateBody, isValidId } = require('../../middlewares');
 
+const { schemas } = require('../../models/contact');
+
+
 router.get('/', ctrl.listOrders)
-router.post('/', ctrl.listOrders)
+router.post('/', validateBody(schemas.addSchema), ctrl.listOrders)
 
 module.exports = router;
