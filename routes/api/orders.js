cb3986b21107190ab432e65eb3794bd6e6dc4687
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-03-08 12:57:48 +0200

    Update orders.js

--- routes/api/orders.js
+++ routes/api/orders.js
@@ -3,11 +3,10 @@ const express = require('express');
 const router = express.Router();
 
 const ctrl = require('../../controllers/orders');
-const { validateBody, isValidId } = require('../../middlewares');
 
 const { Order } = require('../../models/order');
 
 router.get('/', ctrl.listOrders)
-router.post('/', validateBody(Order), ctrl.listOrders)
+router.post('/', ctrl.listOrders)
 
 module.exports = router;
