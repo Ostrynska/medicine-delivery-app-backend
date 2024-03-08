3b077b1ad55d5f6dc5c7d57794f8e53386d8c46b
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-03-08 13:01:02 +0200

    Update orders.js

--- controllers/orders.js
+++ controllers/orders.js
@@ -2,9 +2,18 @@ const Order = require('../models/order');
 const { HttpError, ctrlWrapper } = require("../helpers");
 
 const listOrders = async (req, res) => {
-    const result = await Order.create(req.body, "-createdAt -updatedAt");
-    res.json(result);
-}
+    try {
+        delete req.body.createdAt;
+        delete req.body.updatedAt;
+
+        const result = await Order.create(req.body);
+        res.json(result);
+    } catch (error) {
+        console.error('Error while creating order:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
 
 module.exports = {
     listOrders: ctrlWrapper(listOrders),
